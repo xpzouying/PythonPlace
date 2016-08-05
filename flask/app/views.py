de35c2bfc0fe9e5594245387583fab343f94cd55
Zou, Ying <zouying@megvii.com>
2016-08-05 19:42:53 +0800

    Add middleware for authenticate, add TODO list.
    - TODO: todo list.
    - app/views.py: add middleware
    - run.py: modify localhost to 0.0.0.0

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,7 +1,33 @@
 from app import app
+from flask import request, Response
+from functools import wraps
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, Flask!"
+
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return "Hello Flask!!!"
+
+
+def authenticate(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print "request: ", request.form
+        if (request.form['name'] != "test" or
+                request.form['password'] != "testing"):
+            return Response('Login error!', 401, {'Authenticate': 'Error...'})
+
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+@app.route('/auth', methods=['GET'])
+@authenticate
+def auth():
+    return "Pass though!"
