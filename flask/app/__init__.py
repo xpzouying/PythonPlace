86d87ebef1a940598659eecf50531d0f1477ecdc
Zou, Ying <zouying@megvii.com>
2016-08-05 17:10:10 +0800

    Re-org code struct.
    Add views.py
    Add run.py.
    
    Run the program with: python ./run.py

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,11 +1,4 @@
 from flask import Flask
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello_flask():
-    return "Hello Flask!"
-
-if __name__ == '__main__':
-    app.run()
+from app import views
